50fbf7b520bc8fe71d43c7237eaa45876a7f7dbf
Rakshit Arora <rakshit087@gmail.com>
2023-04-12 20:23:47 +0530

    cleaned code and added data context to app

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,8 @@
 import '@/styles/app.css'
-import '@rainbow-me/rainbowkit/styles.css'
 import { AnimatePresence } from 'framer-motion'
 import Head from 'next/head'
 
 import { Raleway } from '@next/font/google'
-import { RainbowKit } from '@/components/providers/RainbowKit'
 import { useIsMounted } from '@/lib/hooks/useIsMounted'
 
 import Header from '@/components/layout/Header'
@@ -12,7 +10,7 @@ import Footer from '@/components/layout/Footer'
 import MDXProvider from '@/components/providers/MDXProvider'
 import { CurrentSlideProvider } from '@/lib/context/CurrentSlideContext'
 import { ModeProvider } from '@/lib/context/ModeContext'
-
+import { DataProvider } from '../lib/context/DataContext'
 import { APP_CONFIG, AUTHOR_IMAGE_URL, AUTHOR_NAME, AUTHOR_URL, PRESENTATION_DATE, SITE_NAME, SITE_TAGLINE, SITE_TITLE } from '@/lib/constants'
 
 const raleway = Raleway({
@@ -34,25 +32,25 @@ export default function App({ Component, pageProps }) {
       </style>
       <MDXProvider>
         {isMounted && (
-          <RainbowKit>
-            <CurrentSlideProvider>
-              <ModeProvider>
-                <AnimatePresence exitBeforeEnter>
-                  <Head>
-                    <title>{SITE_TITLE}</title>
-                    <link rel="icon" href="/favicon.ico" />
-                    <link
-                      href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap"
-                      rel="stylesheet"
-                    />
-                  </Head>
-                  <Header name={SITE_NAME} title={SITE_TAGLINE} date={PRESENTATION_DATE} url={APP_CONFIG.twitter} />
+          <CurrentSlideProvider>
+            <ModeProvider>
+              <AnimatePresence exitBeforeEnter>
+                <Head>
+                  <title>{SITE_TITLE}</title>
+                  <link rel="icon" href="/favicon.ico" />
+                  <link
+                    href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap"
+                    rel="stylesheet"
+                  />
+                </Head>
+                <Header name={SITE_NAME} title={SITE_TAGLINE} date={PRESENTATION_DATE} url={APP_CONFIG.twitter} />
+                <DataProvider>
                   <Component {...pageProps} />
-                  <Footer authorName={AUTHOR_NAME} authorUrl={AUTHOR_URL} authorImgUrl={AUTHOR_IMAGE_URL} />
-                </AnimatePresence>
-              </ModeProvider>
-            </CurrentSlideProvider>
-          </RainbowKit>
+                </DataProvider>
+                <Footer authorName={AUTHOR_NAME} authorUrl={AUTHOR_URL} authorImgUrl={AUTHOR_IMAGE_URL} />
+              </AnimatePresence>
+            </ModeProvider>
+          </CurrentSlideProvider>
         )}
       </MDXProvider>
     </>

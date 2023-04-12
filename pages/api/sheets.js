49d954e519986e30dc458d0ff26283011f3a2d35
Rakshit Arora <rakshit087@gmail.com>
2023-04-13 01:55:32 +0530

    fixed data type

--- pages/api/sheets.js
+++ pages/api/sheets.js
@@ -17,8 +17,74 @@ export async function getDataFromSheets() {
     })
 
     const rows = response.data.values
+    let sendThis = [
+      { title: 'timestamp', responses: [] },
+      {
+        title: 'What is the age group of working mom you interviewed',
+        responses: [],
+      },
+      {
+        title: 'What is the age group of the children she had?',
+        responses: [],
+      },
+      {
+        title: 'Did you ever face the Working Mom Dilemma?',
+        responses: [],
+      },
+      {
+        title: 'Did you ever face any biases at the workplace due to your maternal role?',
+        responses: [],
+      },
+      {
+        title: 'What changes did you experience in your role during and after COVID-19 Pandemic?',
+        responses: [],
+      },
+      {
+        title: 'Did you make any compromises with your health?',
+        responses: [],
+      },
+      {
+        title:
+          'Do you have to make compromises with your dignity in order to ask for help in order to ensure that your role as a mother and as a worker is performed effectively?',
+        responses: [],
+      },
+      {
+        title: 'Do you come across people in your day today who constantly undermine your efforts and expect more from you?',
+        responses: [],
+      },
+      {
+        title: 'How do you deal with such people?',
+        responses: [],
+      },
+      {
+        title: 'How do you deal with a situation where you think you made a mistake as a mother?',
+        responses: [],
+      },
+      {
+        title: 'How do you think role of Indian working mothers are different from that mothers in western countries?',
+        responses: [],
+      },
+      {
+        title: 'Do you think women staying at home have better sense and time of living life?',
+        responses: [],
+      },
+      {
+        title: 'Do you think stay-at-home mothers are way more effective mothers?',
+        responses: [],
+      },
+      {
+        title: 'If at all you have to choose between a working professional and a stay at home mother, which role would you select? ',
+        responses: [],
+      },
+    ]
     if (rows.length) {
-      return rows
+      for (let i = 1; i < rows.length; i++) {
+        const row = rows[i]
+        for (let j = 0; j < row.length; j++) {
+          sendThis[j].responses.push(row[j])
+        }
+      }
+      return sendThis
     }
   } catch (err) {
     console.log(err)
